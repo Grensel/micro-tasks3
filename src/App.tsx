c523546c6904ecbfea105584c0cb9f1ac380c2f7
Grensel <sergeiodinochenko@mail.ru>
2025-02-26 12:58:23 +0300

    feat: completed first micro task 3 spring

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from "./components/Site.module.css";
+import { PageOne } from './components/pages/PageOne';
+import { PageTwo } from './components/pages/PageTwo';
+import { PageThree } from './components/pages/PageThree';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { Error404 } from './components/pages/Error404';
+import { S } from './components/Site.styles';
+
+
+export const App = () => {
+    const PATH = {
+        PAGE1: '/page1',
+        PAGE2: '/page2',
+        PAGE3: '/page3',
+        ERROR: '/*'
+    } as const
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <nav>
+                        <S.NavWrapper>
+                            <NavLink to={PATH.PAGE1} >Page1</NavLink>
+                        </S.NavWrapper>
+                        <S.NavWrapper>
+                            <NavLink to={PATH.PAGE2} >Page2</NavLink>
+                        </S.NavWrapper>
+                        <S.NavWrapper>
+                            <NavLink to={PATH.PAGE3} >Page3</NavLink>
+                        </S.NavWrapper>
+                    </nav>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/' element={<Navigate to={PATH.PAGE1} />} />
+                        <Route path={PATH.PAGE1} element={<PageOne />} />
+                        <Route path={PATH.PAGE2} element={<PageTwo />} />
+                        <Route path={PATH.PAGE3} element={<PageThree />} />
+                        <Route path={PATH.ERROR} element={<Error404 />} />
+                    </Routes>
+                </div>
+            </div >
+            <div className={styles.footer}>abibas 2023</div>
+        </div >
+    );
+}
