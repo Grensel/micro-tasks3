1f2457c6b7936ef5eeebd6394b8db3d94f8f0f83
Grensel <sergeiodinochenko@mail.ru>
2025-02-26 13:03:27 +0300

    Deploying to GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode >
-    <BrowserRouter basename="micro-tasks3">
+    <HashRouter basename="micro-tasks3">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
